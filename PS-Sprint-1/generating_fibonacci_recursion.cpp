5d43143dc28814230b6e5c079aff32bb70ea7a7a
theweich <dagisanz@gmail.com>
2026-01-18 09:23:16 +0100

    Generating a Fibonacci Sequence Using Recursion

---
+++ PS-Sprint-1/generating_fibonacci_recursion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int);
+
+int main(int argc, char const *argv[])
+{
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << fibonacci(i) << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
